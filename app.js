60c0a3c9a54234d02daed0c76ff78ad565131948
aymensp <aymen.smati@esprit.tn>
2021-11-05 18:12:28 +0100

    'added db"

--- app.js
+++ app.js
@@ -1,43 +1,51 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var eventRouter = require('./routes/event.route');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var eventRouter = require("./routes/event.route");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'twig');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "twig");
 
-app.use(logger('dev'));
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/event', eventRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/event", eventRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
-
+mongoose.connect("mongodb://localhost:27017/5SIM3")
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
