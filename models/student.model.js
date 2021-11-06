31d4160b415953520111743a48607b7fffeabebf
aymensp <aymen.smati@esprit.tn>
2021-11-06 18:54:43 +0100

    model added

--- models/student.model.js
+++ models/student.model.js
@@ -1,3 +1,23 @@
 var mongoose = require("mongoose");
 
-const studentSchema = mongoose.Schema({},{})
+const studentSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
+  },
+  {
+      timestamps : true
+  }
+);
+
+export const Student = mongoose.model("student",studentSchema);
